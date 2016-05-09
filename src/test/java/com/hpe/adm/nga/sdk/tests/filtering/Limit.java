82c14ae5e41efae899d2f55694b08ec133636461
perach <cohen.perach@hpe.com>
2016-05-09 11:10:39 +0300

    limit test

---
+++ src/test/java/com/hpe/adm/nga/sdk/tests/filtering/Limit.java
@@ -0,0 +1,40 @@
+package com.hpe.adm.nga.sdk.tests.filtering;
+
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.tests.base.TestBase;
+import com.hpe.adm.nga.sdk.utils.CommonUtils;
+import com.hpe.adm.nga.sdk.utils.generator.DataGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+
+/**
+ * Created by perach on 08/05/2016.
+ */
+public class Limit extends TestBase {
+
+    public Limit() {
+        entityName = "releases";
+    }
+
+    @Test
+    public void testLimit() throws Exception {
+
+        Collection<EntityModel> generatedEntity = DataGenerator.generateEntityModelCollection(nga, entityName);
+        entityList.create().entities(generatedEntity).execute();
+
+        Collection<EntityModel> getAllEntities = entityList.get().execute();
+        int totalCount = getAllEntities.size();
+
+        if (totalCount > 1) {
+
+            Collection<EntityModel> getLimitEntities = entityList.get().limit(totalCount - 1).execute();
+            int limit = getLimitEntities.size();
+
+            Assert.assertTrue(limit + 1 == totalCount);
+        }
+
+    }
+
+}
