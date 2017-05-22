9a1e523793ab98bec8c8b4081f72a442f68cbb27
Andras Toth <atoth@hpe.com>
2017-05-22 16:41:13 +0300

    Copyright update

--- sdk-integration-tests/src/test/java/com/hpe/adm/nga/sdk/tests/filtering/TestLimit.java
+++ sdk-integration-tests/src/test/java/com/hpe/adm/nga/sdk/tests/filtering/TestLimit.java
@@ -1,6 +1,7 @@
 /*
  *
- *    Copyright 2017 Hewlett-Packard Development Company, L.P.
+ *
+ *    Copyright 2017 Hewlett-Packard Enterprise Development Company, L.P.
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -12,6 +13,7 @@
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
+ *
  */
 package com.hpe.adm.nga.sdk.tests.filtering;
 
