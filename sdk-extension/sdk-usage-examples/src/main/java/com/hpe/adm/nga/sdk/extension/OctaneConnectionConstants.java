50a94b1f83db75f56d7b880fa45302c8f2cf221b
Andras Toth <atoth@hpe.com>
2017-08-29 09:50:36 +0300

    Removed hardcoded url form connection constants

--- sdk-extension/sdk-usage-examples/src/main/java/com/hpe/adm/nga/sdk/extension/OctaneConnectionConstants.java
+++ sdk-extension/sdk-usage-examples/src/main/java/com/hpe/adm/nga/sdk/extension/OctaneConnectionConstants.java
@@ -21,12 +21,12 @@ package com.hpe.adm.nga.sdk.extension;
 public interface OctaneConnectionConstants {
 
     //Server
-    String urlDomain = "http://myd-vm10632.hpeswlab.net:8081";
-    Long sharedspaceId = 1001L;
-    Long workspaceId = 1002L;
+    String urlDomain = "OCTANE_URL";
+    Long sharedspaceId = -1L;
+    Long workspaceId = -1L;
 
     //Auth
-    String username = "sa@nga";
-    String password = "Welcome1";
+    String username = "username";
+    String password = "password";
 
 }
