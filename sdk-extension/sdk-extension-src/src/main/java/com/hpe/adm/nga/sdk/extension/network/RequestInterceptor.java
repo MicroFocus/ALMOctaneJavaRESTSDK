a5abec8f557f6de32f763b0aef2116202bc3686a
Andras Toth <atoth@hpe.com>
2017-08-29 10:21:11 +0300

    Added javadoc to RequestInterceptor

--- sdk-extension/sdk-extension-src/src/main/java/com/hpe/adm/nga/sdk/extension/network/RequestInterceptor.java
+++ sdk-extension/sdk-extension-src/src/main/java/com/hpe/adm/nga/sdk/extension/network/RequestInterceptor.java
@@ -17,11 +17,33 @@ package com.hpe.adm.nga.sdk.extension.network;
 
 import java.util.Map;
 
+/**
+ * Interceptor for http requests
+ * Methods are called before request is passed to the {@link com.hpe.adm.nga.sdk.network.OctaneHttpClient} implementation
+ */
 public interface RequestInterceptor {
+
+    /**
+     * Get and change the url before the request is executed
+     * @param url original url
+     * @return new url that is passed {@link com.hpe.adm.nga.sdk.network.OctaneHttpClient} implementation
+     */
     default String url(String url){
         return url;
     }
+
+    /**
+     * Get and change the content before the request is executed
+     * @param content original content of the request
+     * @return new content that is passed {@link com.hpe.adm.nga.sdk.network.OctaneHttpClient} implementation
+     */
     default String content(String content){ return content; }
+
+    /**
+     * Get and change the headers before the request is executed
+     * @param headers headers of the original request
+     * @return new headers that are passed {@link com.hpe.adm.nga.sdk.network.OctaneHttpClient} implementation
+     */
     default Map<String, Object> headers(Map<String, Object> headers){
         return headers;
     }
