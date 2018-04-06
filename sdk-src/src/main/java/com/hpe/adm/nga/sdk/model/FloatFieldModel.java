34b998f34c48e61d712cb36bcb5e168927802796
Andras Toth <atoth@hpe.com>
2018-04-06 15:34:36 +0300

    Added equals method for all FieldModels

--- sdk-src/src/main/java/com/hpe/adm/nga/sdk/model/FloatFieldModel.java
+++ sdk-src/src/main/java/com/hpe/adm/nga/sdk/model/FloatFieldModel.java
@@ -41,4 +41,36 @@ public class FloatFieldModel implements FieldModel<Float> {
     public String getName() {
         return name;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FloatFieldModel other = (FloatFieldModel) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+    
 }
