8adc807c983a7febc403297ea06c8a8072924bb5
Andras Toth <atoth@hpe.com>
2018-04-17 18:48:24 +0300

    Added entity util class with a bunch of nice static methods

---
+++ sdk-src/src/main/java/com/hpe/adm/nga/sdk/model/EntityUtil.java
@@ -0,0 +1,167 @@
+package com.hpe.adm.nga.sdk.model;
+
+import java.util.*;
+
+public class EntityUtil {
+
+    /**
+     * Interface for checking equality between two entity models
+     */
+    public interface EntityEquator {
+        boolean areEqual(EntityModel leftEntityModel, EntityModel rightEntityModel);
+    }
+
+    /**
+     * Check if two entities are equal by id + type
+     *
+     * @param leftEntityModel  {@link EntityModel}
+     * @param rightEntityModel {@link EntityModel}
+     * @return true if the two params have the same id and type, false otherwise
+     */
+    public static boolean areEqualByIdAndType(EntityModel leftEntityModel, EntityModel rightEntityModel) {
+        return Objects.equals(leftEntityModel.getType(), rightEntityModel.getType()) &&
+                Objects.equals(leftEntityModel.getId(), rightEntityModel.getId());
+    }
+
+    /**
+     * Check if two entities have exactly the same field models, not taking into account their order
+     *
+     * @param leftEntityModel  {@link EntityModel}
+     * @param rightEntityModel {@link EntityModel}
+     * @return true if the two params have the same field models, false otherwise
+     */
+    public static boolean areEqualByContent(EntityModel leftEntityModel, EntityModel rightEntityModel) {
+
+        Set<FieldModel> leftFieldModels = leftEntityModel.getValues();
+        Set<FieldModel> rightFieldModels = rightEntityModel.getValues();
+
+        if(leftFieldModels.size() != rightFieldModels.size()){
+            return false;
+        }
+        for (FieldModel leftFieldModel : leftFieldModels) {
+            FieldModel rightFieldModel = findByFieldName(rightFieldModels, leftFieldModel.getName());
+            if (rightFieldModel == null) {
+                return false;
+            }
+            else if(!areFieldModelsEqualByContent(leftFieldModel, rightFieldModel)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean areFieldModelsEqualByContent(FieldModel leftFieldModel, FieldModel rightFieldModel) {
+        if(!Objects.equals(leftFieldModel.getName(), rightFieldModel.getName())){
+            return false;
+        }
+        if(!leftFieldModel.getClass().equals(rightFieldModel.getClass())) {
+            return false;
+        }
+        if(leftFieldModel instanceof ReferenceFieldModel && rightFieldModel instanceof ReferenceFieldModel) {
+            EntityModel refLeftEntityModel = (EntityModel) leftFieldModel.getValue();
+            EntityModel refRightEntityModel = (EntityModel) rightFieldModel.getValue();
+            return areEqualByContent(refLeftEntityModel, refRightEntityModel);
+        }
+        if(leftFieldModel instanceof MultiReferenceFieldModel && rightFieldModel instanceof MultiReferenceFieldModel) {
+            Collection<EntityModel> refLeftEntityModel = (Collection<EntityModel>) leftFieldModel.getValue();
+            Collection<EntityModel> refRightEntityModel = (Collection<EntityModel>) rightFieldModel.getValue();
+            return containsSameEntities(refLeftEntityModel, refRightEntityModel, EntityUtil::areEqualByContent);
+        }
+        //simple field, just equals the value
+        return Objects.equals(leftFieldModel.getValue(), rightFieldModel.getValue());
+    }
+
+    private static FieldModel findByFieldName(Collection<FieldModel> collection, String fieldName) {
+        return collection
+            .stream()
+            .filter(fieldModel -> fieldName.equals(fieldModel.getName()))
+            .findFirst()
+            .orElse(null);
+    }
+
+    /**
+     * Check if a collection contains and entity model, uses {@link #areEqualByIdAndType(EntityModel, EntityModel)}
+     *
+     * @param collection  collection of entity models
+     * @param entityModel target entity model
+     * @return true if the entity is in the collection, false otherwise
+     */
+    public static boolean containsEntityModel(Collection<EntityModel> collection, EntityModel entityModel) {
+        return containsEntityModel(collection, entityModel, EntityUtil::areEqualByIdAndType);
+    }
+
+    /**
+     * Check if a collection contains and entity model, can use any {@link EntityEquator}
+     *
+     * @param collection  collection of entity models
+     * @param entityModel target entity model
+     * @return true if the entity is in the collection, false otherwise
+     */
+    public static boolean containsEntityModel(Collection<EntityModel> collection, EntityModel entityModel, EntityEquator entityEquator) {
+        for (EntityModel em : collection) {
+            if (entityEquator.areEqual(em, entityModel)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if baseCollection contains all elements of sourceCollection
+     * @param baseCollection baseCollection
+     * @param sourceCollection sourceCollection
+     * @param entityEquator {@link EntityEquator} to determine equality
+     * @return true if all elements of sourceCollection are present in baseCollection
+     */
+    public static boolean containsAllEntityModels(Collection<EntityModel> baseCollection, Collection<EntityModel> sourceCollection, EntityEquator entityEquator) {
+        for (EntityModel em : sourceCollection) {
+            if (!containsEntityModel(baseCollection, em, entityEquator)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if two entity collections contain the same entities
+     * @param leftCollection collection of {@link EntityModel}
+     * @param rightCollection collection of {@link EntityModel}
+     * @param entityEquator {@link EntityEquator} to use to determine equality
+     * @return true if the two collections contain the same entities, false otherwise
+     */
+    public static boolean containsSameEntities(Collection<EntityModel> leftCollection, Collection<EntityModel> rightCollection, EntityEquator entityEquator) {
+        return  leftCollection.size() == rightCollection.size() &&
+                containsAllEntityModels(leftCollection, rightCollection, entityEquator) &&
+                containsAllEntityModels(rightCollection, leftCollection, entityEquator);
+    }
+
+    /**
+     * Remove an entity model from a collection, uses {@link #areEqualByIdAndType(EntityModel, EntityModel)}
+     *
+     * @param collection  collection of entity models
+     * @param entityModel target entity model
+     * @return true if the entity has been removed, false otherwise
+     */
+    public static boolean removeEntityModel(Collection<EntityModel> collection, EntityModel entityModel) {
+        return removeEntityModel(collection, entityModel, EntityUtil::areEqualByIdAndType);
+    }
+
+    /**
+     * Remove an entity model from a collection, can use any {@link EntityEquator}
+     *
+     * @param collection  collection of entity models
+     * @param entityModel target entity model
+     * @return true if the entity has been removed, false otherwise
+     */
+    public static boolean removeEntityModel(Collection<EntityModel> collection, EntityModel entityModel, EntityEquator entityEquator) {
+        List<EntityModel> toBeRemoved = new ArrayList<>();
+        for (EntityModel em : collection) {
+            if (entityEquator.areEqual(em, entityModel)) {
+                toBeRemoved.add(em);
+            }
+        }
+        return collection.removeAll(toBeRemoved);
+    }
+
+}
