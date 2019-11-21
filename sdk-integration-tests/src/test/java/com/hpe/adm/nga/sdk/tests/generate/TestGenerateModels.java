5cfb2200a4a60a9c24253023d64728eb60c09e43
Andras Toth <atoth@microfocus.com>
2019-11-21 15:25:01 +0200

    Adding missing license header.

--- sdk-integration-tests/src/test/java/com/hpe/adm/nga/sdk/tests/generate/TestGenerateModels.java
+++ sdk-integration-tests/src/test/java/com/hpe/adm/nga/sdk/tests/generate/TestGenerateModels.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2017 EntIT Software LLC, a Micro Focus company, L.P.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hpe.adm.nga.sdk.tests.generate;
 
 import com.hpe.adm.nga.sdk.Octane;
@@ -5,7 +18,8 @@ import com.hpe.adm.nga.sdk.authentication.Authentication;
 import com.hpe.adm.nga.sdk.entities.TypedEntityList;
 import com.hpe.adm.nga.sdk.entities.get.GetTypedEntity;
 import com.hpe.adm.nga.sdk.generate.GenerateModels;
-import com.hpe.adm.nga.sdk.model.*;
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.TypedEntityModel;
 import com.hpe.adm.nga.sdk.utils.AuthenticationUtils;
 import com.hpe.adm.nga.sdk.utils.CommonUtils;
 import com.hpe.adm.nga.sdk.utils.ConfigurationUtils;
